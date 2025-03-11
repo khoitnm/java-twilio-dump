9cbb8b815a75bd28b1a53b57e868cc86f588d499
trank <Kevin.Tran@pointclickcare.com>
2025-03-11 17:19:19 -0400

    Add test data.

---
+++ src/test/java/com/mycompany/myapp/ConversationCreatorAppTest.java
@@ -0,0 +1,18 @@
+package com.mycompany.myapp;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class ConversationCreatorAppTest {
+    @Test
+    public void testMain() throws IOException {
+        // Arrange
+        String[] args = new String[2];
+        args[0] = "C:\\dev\\workspace\\personal\\java-twilio-dump\\src\\main\\resources\\input-conversations.txt";
+        args[1] = "C:\\dev\\workspace\\personal\\java-twilio-dump\\target\\output.json";
+        // Act
+        ConversationCreatorApp.main(args);
+        // Assert
+    }
+}
